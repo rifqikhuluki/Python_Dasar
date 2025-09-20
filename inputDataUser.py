af4c744040a3233f377bebee921d2311f5e9b739
rifqikhuluki <rifkikuluki09@gmail.com>
2025-09-20 22:10:31 +0700

    Input Data User Python

---
+++ inputDataUser.py
@@ -0,0 +1,10 @@
+#input data dari user
+
+# data = input("Masukkan data = ")
+# print(data, type(data))
+
+#mencari luas persegi panjang
+panjang = float(input("masukkan panjang = "))
+lebar = float(input("masukkan lebar = "))
+luas = panjang*lebar
+print(luas)
