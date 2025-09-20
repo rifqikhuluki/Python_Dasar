ffa834bc3cd499885438943c06563832c93eaa17
rifqikhuluki <rifkikuluki09@gmail.com>
2025-09-20 22:09:46 +0700

    Logika Boolean Python

---
+++ logikaBoolean.py
@@ -0,0 +1,21 @@
+a = False
+b = True
+
+#not
+hasilNot = not a
+print(hasilNot)
+
+#or
+hasilOr = a or b
+print(hasilOr)
+
+#and
+hasilAnd = a and b
+print(hasilAnd)
+
+#xor(akan true jika hanya ada satu pembanding true, maka jika true ^ true = false)
+hasilXorA = a ^ b
+hasilXorB = b ^ b
+print(hasilXorA)
+print(hasilXorB)
+
