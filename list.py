4534114bc06f6a47a8c135eef09331d7f70419e0
rifqikhuluki <rifkikuluki09@gmail.com>
2025-11-27 22:05:32 +0700

    List

---
+++ list.py
@@ -0,0 +1,23 @@
+# List
+
+#kumpulan data numbers
+data_angka = [1,3,5,7,9]
+print(data_angka)
+
+#kumpulan data string
+data_str = ["cook","gento","ndlogok"]
+print(data_str)
+
+#kumpulan data bool
+data_bool = [True, False, True, False]
+print(data_bool)
+
+#kumpulan campuran
+data_campuran = [1, "pentol", 2, "sempol", "ucup"]
+print(data_campuran)
+
+# cara alternatif membuat list
+data_range = range(0,10)
+print(data_range)
+data_list = list(data_range)
+print(data_list)
